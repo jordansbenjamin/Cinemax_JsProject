26f7fec52e619fc2c0b7c4658e3864c1a4b30926
jordansbenjamin <14327@coderacademy.edu.au>
2023-09-11 18:45:47 +1000

    Implemented logic for displaying popular tv shows

--- js/script.js
+++ js/script.js
@@ -54,6 +54,32 @@ async function displayPopularMovies() {
 	});
 }
 
+// Function for displaying popular tv shows
+async function displayPopularShows() {
+	const { results } = await fetchAPIData("tv/popular");
+	// console.log(results);
+
+	results.forEach((show) => {
+		const div = document.createElement("div");
+		div.classList.add("card");
+		div.innerHTML = `
+            <a href="tv-details.html?id=${show.id}">
+                ${
+                    show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}" />` : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}" />`
+                }
+            </a>
+            <div class="card-body">
+                <h5 class="card-title">${show.name}</h5>
+                <p class="card-text">
+                    <small class="text-muted">Air Date: ${show.first_air_date}</small>
+                </p>
+            </div>			
+        `;
+
+        document.querySelector('#popular-shows').appendChild(div);
+	});
+}
+
 // Building a simple router
 // To run specific javascript / functions on specific pages
 
@@ -76,6 +102,7 @@ function init() {
 			break;
 		case "/shows.html":
 			console.log("Shows");
+            displayPopularShows();
 			break;
 		case "/movie-details.html":
 			console.log("Movie Details");
