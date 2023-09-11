10222af2849269d290a7ae2884c816b2f44301a5
jordansbenjamin <14327@coderacademy.edu.au>
2023-09-11 19:42:46 +1000

    Implemented slider functionality with swiper library

--- js/script.js
+++ js/script.js
@@ -217,6 +217,53 @@ function addCommasToNumber(number) {
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
+// Display slider movies
+async function displaySlider() {
+	const {results} = await fetchAPIData('movie/now_playing');
+
+	console.log(results);
+
+	results.forEach(movie => {
+		const div = document.createElement('div');
+
+		div.classList.add('swiper-slide');
+		div.innerHTML = `
+		<a href="movie-details.html?id=${movie.id}">
+			<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+		</a>
+		<h4 class="swiper-rating"><i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10</h4>
+		`;
+
+		document.querySelector('.swiper-wrapper').appendChild(div);
+
+		initSwiper();
+	})
+}
+
+function initSwiper() {
+	const swiper = new Swiper('.swiper', {
+		slidesPerView: 1,
+		spaceBetween: 30,
+		freeMode: true,
+		loop: true,
+		autoplay: {
+			delay: 4000,
+			disableOnInteraction: false
+		},
+		breakpoints: {
+			500: {
+				slidesPerView: 2
+			},
+			700: {
+				slidesPerView: 3
+			},
+			1200: {
+				slidesPerView: 4
+			}
+		}
+	})
+}
+
 // Building a simple router
 // To run specific javascript / functions on specific pages
 
@@ -236,6 +283,7 @@ function init() {
 		case "/index.html":
 			console.log("Home");
 			displayPopularMovies();
+			displaySlider();
 			break;
 		case "/shows.html":
 			console.log("Shows");
