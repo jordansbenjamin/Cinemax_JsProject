930c49770577f63214b336423d3d7d4318fc6100
jordansbenjamin <14327@coderacademy.edu.au>
2023-09-11 19:02:23 +1000

    Started implementing logic for displaying movie details

--- js/script.js
+++ js/script.js
@@ -80,6 +80,56 @@ async function displayPopularShows() {
 	});
 }
 
+// Display movie details
+async function displayMovieDetails() {
+    // window.location.search retrieves the query string part of the URL
+    // Where the movie id is
+    const movieId = window.location.search.split('=')[1];
+
+    const movie = await fetchAPIData(`movie/${movieId}`);
+
+    const div = document.createElement('div');
+    
+    div.innerHTML = `
+    <div class="details-top">
+					<div>
+                    ${
+                        movie.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" class="card-img-top" alt="${movie.title}" />` : `<img src="images/no-image.jpg" class="card-img-top" alt="${movie.title}" />`
+                    }
+					</div>
+					<div>
+						<h2>${movie.title}</h2>
+						<p>
+							<i class="fas fa-star text-primary"></i>
+							${movie.vote_average.toFixed(1)}/ 10
+						</p>
+						<p class="text-muted">Release Date: ${movie.release_date}</p>
+						<p>
+							${movie.overview}
+						</p>
+						<h5>Genres</h5>
+						<ul class="list-group">
+							${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+						</ul>
+						<a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+					</div>
+				</div>
+				<div class="details-bottom">
+					<h2>Movie Info</h2>
+					<ul>
+						<li><span class="text-secondary">Budget:</span> ${movie.budget}</li>
+						<li><span class="text-secondary">Revenue:</span> $2,000,000</li>
+						<li><span class="text-secondary">Runtime:</span> 90 minutes</li>
+						<li><span class="text-secondary">Status:</span> Released</li>
+					</ul>
+					<h4>Production Companies</h4>
+					<div class="list-group">Company 1, Company 2, Company 3</div>
+				</div>
+    `;
+    document.querySelector('#movie-details').appendChild(div);
+    console.log(movieId);
+}
+
 // Building a simple router
 // To run specific javascript / functions on specific pages
 
@@ -106,6 +156,7 @@ function init() {
 			break;
 		case "/movie-details.html":
 			console.log("Movie Details");
+            displayMovieDetails();
 			break;
 		case "/tv-details.html":
 			console.log("TV Details");
