954ee08b711a381f0eab39827512fb6b0ba48656
jordansbenjamin <14327@coderacademy.edu.au>
2023-09-11 18:06:45 +1000

    Added initial logic for routers

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+// Building a simple router
+// To run specific javascript / functions on specific pages
+
+// console.log(window.location.pathname);
+
+// global state variable
+const global = {
+	currentPage: window.location.pathname,
+};
+// console.log(global.currentPage);
+
+// Initialise app
+// Simple router logic for executing function required for each specified page
+function init() {
+    switch (global.currentPage) {
+        case '/':
+            console.log('Home');
+            break;
+        case '/shows.html':
+            console.log('Shows');
+            break;
+        case '/movie-details.html':
+            console.log('Movie Details');
+            break;
+        case '/tv-details.html':
+            console.log('TV Details');
+            break;
+        case '/search.html':
+            console.log('Search');
+            break;
+    }
+}
+
+// Only gets triggered when all the DOM elements are loaded on the page for the init func to execute
+// Basically when the initial HTML document has been completely loaded and parsed
+document.addEventListener('DOMContentLoaded', init);
