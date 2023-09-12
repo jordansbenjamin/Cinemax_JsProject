1e2f0ef7495d9f1608441304fe57d82f229d5a1e
jordansbenjamin <14327@coderacademy.edu.au>
2023-09-12 13:20:31 +1000

    Almost finished with implementing pagination, but fixed markup issue and styling

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ async function searchAPIData(endpoint) {
     // Display spinner before/during data being fetched
     showSpinner()
 
-	const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-UK&query=${global.search.term}`);
+	const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-UK&query=${global.search.term}&page=${global.search.page}`);
 
 	const data = await response.json();
 
@@ -71,6 +71,12 @@ async function search() {
 
 // Function to display search results
 function displaySearchResults(results) {
+
+	// Clear previous results
+	document.querySelector('#search-results').innerHTML = '';
+	document.querySelector('#search-results-heading').innerHTML = '';
+	document.querySelector('#pagination').innerHTML = '';
+
 	results.forEach((result) => {
 		const div = document.createElement("div");
 		div.classList.add("card");
@@ -94,6 +100,38 @@ function displaySearchResults(results) {
 
         document.querySelector('#search-results').appendChild(div);
 	});
+
+	displayPagination();
+}
+
+// Create & display pagination for search functionality
+function displayPagination() {
+	const div = document.createElement('div');
+	div.classList.add('pagination');
+	div.innerHTML = `
+	<button class="btn btn-primary" id="prev">Prev</button>
+	<button class="btn btn-primary" id="next">Next</button>
+	<div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+	`;
+
+	document.querySelector('#pagination').appendChild(div);
+
+	// Disable previous button if on first page
+	if (global.search.page === 1) {
+		document.querySelector('#prev').disabled = true;
+	}
+
+	// Disable next button if on last page
+	if (global.search.page === global.search.totalPages) {
+		document.querySelector('#next').disabled = true;
+	}
+	
+	// Next page
+	document.querySelector('#next').addEventListener('click', async () => {
+		global.search.page++;
+		const { results, total_pages } = await searchAPIData();
+		displaySearchResults(results);
+	})
 }
 
 // Displaying spinner func
